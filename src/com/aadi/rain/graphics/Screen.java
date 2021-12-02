25c20d7e4175fae6d643825c2b77aab5b2c864e5
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-03 01:27:55 +0530

    Rendering Pixels

--- src/com/aadi/rain/graphics/Screen.java
+++ src/com/aadi/rain/graphics/Screen.java
@@ -4,12 +4,22 @@ package com.aadi.rain.graphics;
 public class Screen {
 
 	private int width, height;
-    private int[] pixels;
+    public int[] pixels;
 
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
+    }
+
+    public void render() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixels[x + y * width] = 0xff00ff; // Index of the array shows the Coordinate System of the Screen
+                //pixels[20 + 30 * width] = 0xff00ff;
+                //pixels[40+ 30 * width] = 0xff00ff;
+            }
+        }
     }
 
 }
