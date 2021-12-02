799e49d85758c2beba06c62b58672ba56a218c65
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-03 00:20:19 +0530

    Porper Comments

--- src/com/aadi/rain/Game.java
+++ src/com/aadi/rain/Game.java
@@ -22,22 +22,24 @@ public class Game extends Canvas implements Runnable {
 	private boolean running = false;
 
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	
+	// Array to store the colors of every pixel of the image which are stored in Hexadecimal format
 	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
 	public Game() {
-		Dimension size = new Dimension(width * scale, height * scale);
+		Dimension size = new Dimension(width * scale, height * scale); // Size of the window
 		setPreferredSize(size);
 
-		frame = new JFrame();
+		frame = new JFrame(); // Initializes a new Window
 	}
 
-	public synchronized void start() {
+	public synchronized void start() { // Starts the thread which are series of events that are executed in the background
 		running = true;
 		thread = new Thread(this, " Display");
 		thread.start();
 	}
 
-	public synchronized void stop() {
+	public synchronized void stop() { // Stops the thread which are series of events that are executed in the background
 		running = false;
 
 		try {
@@ -47,7 +49,7 @@ public class Game extends Canvas implements Runnable {
 		}
 	}
 
-	public void run() {
+	public void run() { // Automatically called on Thread Start
 		while (running) {
 			update();
 			render();
@@ -58,9 +60,15 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void render() {
+		// It is used to create a buffer strategy for the canvas which is used to render the image on the screen
+		// BufferStrategy creates a temporary storage in the RAM which stores all the information of the colors to be presented on the screen
+		// It stores the hexadecimal values of the colors to be presented on the screen in the next frame
 		BufferStrategy bs = getBufferStrategy();
 
 		if (bs == null) {
+			// We use the value 3 as it is highly opimal Value because
+			// first values is that value which is currently beign presented on the screen
+			// Second and Third Value are the 2 buffers that are already created to be presented on the screen in the next frame
 			createBufferStrategy(3);
 			return;
 		}
@@ -75,6 +83,7 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String[] args) {
 		Game game = new Game();
 
+		// Specifications for the Window
 		game.frame.setResizable(false);
 		game.frame.setTitle("Rain");
 		game.frame.add(game);
