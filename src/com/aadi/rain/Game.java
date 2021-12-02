e3f6dfe0a446ec525e4652f1cee1198ca3fa499f
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-02 23:50:38 +0530

    Buffered Image and Rasters

--- src/com/aadi/rain/Game.java
+++ src/com/aadi/rain/Game.java
@@ -1,22 +1,28 @@
 package com.aadi.rain;
 
-import javax.swing.JFrame;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Color;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
-	
+
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
@@ -24,19 +30,19 @@ public class Game extends Canvas implements Runnable {
 
 		frame = new JFrame();
 	}
-	
+
 	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, " Display");
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
 		running = false;
-		
+
 		try {
 			thread.join();
-		}catch(InterruptedException e) {
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
@@ -53,12 +59,12 @@ public class Game extends Canvas implements Runnable {
 
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
-		
+
 		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
