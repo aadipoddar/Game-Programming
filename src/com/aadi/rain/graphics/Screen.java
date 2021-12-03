177a1ee19c3842283a6fe35147b4f5bdaa9de333
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-03 15:27:43 +0530

    Tiles

--- src/com/aadi/rain/graphics/Screen.java
+++ src/com/aadi/rain/graphics/Screen.java
@@ -1,18 +1,25 @@
 package com.aadi.rain.graphics;
 
+import java.util.Random;
+
 // This class Fills pixels in with the colors that we specify
 public class Screen {
 
 	private int width, height;
 	public int[] pixels;
 
-	int xtime = 100, ytime = 50;
-	int counter = 0;
+	public int[] tiles = new int[64 * 64];
+
+	private Random random = new Random();
 
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
+
+		for (int i = 0; i < 64 * 64; i++) {
+			tiles[i] = random.nextInt(0xffffff);
+		}
 	}
 
 	public void clear() {
@@ -22,20 +29,18 @@ public class Screen {
 	}
 
 	public void render() {
-		counter++;
-
-		if (counter % 100 == 0) xtime--;
-		if (counter % 100 == 0) ytime--;
 
 		for (int y = 0; y < height; y++) {
 			// Deals With Out of Bounds Exception when the pixel goes beyond the size of the frame in the y axis
-			if (ytime < 0 || ytime >= height) break;
+			if (y < 0 || y >= height) break;
 
 			for (int x = 0; x < width; x++) {
 				// Deals With Out of Bounds Exception when the pixel goes beyond the size of the frame in the x axis
-				if (xtime < 0 || xtime >= width) break;
+				if (x < 0 || x >= width) break;
 
-				pixels[xtime + ytime * width] = 0xff00ff; // Index of the array shows the Coordinate System of the Screen
+				int tileIndex = (x >> 4) + (y >> 4) * 64; // same as this { int tileIndex = (x / 16) + (y / 16) * 64; } using bitwise operator				
+
+				pixels[x + y * width] = tiles[tileIndex]; // Index of the array shows the Coordinate System of the Screen
 			}
 		}
 	}
