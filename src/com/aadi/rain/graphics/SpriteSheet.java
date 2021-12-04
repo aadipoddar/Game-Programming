69f9a84ac8dc29723e5e38964d788d1a79560d42
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-05 02:07:17 +0530

    Sprite Sheets

---
+++ src/com/aadi/rain/graphics/SpriteSheet.java
@@ -0,0 +1,33 @@
+package com.aadi.rain.graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+// This class is in charge of any sprite sheet that we have and caching them in the memory
+public class SpriteSheet {
+
+	private String path;
+	private final int SIZE;
+	public int[] pixels;
+
+	public SpriteSheet(String path, int size) {
+		this.path = path;
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		load();
+	}
+
+	private void load() {
+		try {
+			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
