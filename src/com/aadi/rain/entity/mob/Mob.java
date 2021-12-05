5378a9bb9946099228022473524fce2dc110d32e
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-06 00:49:03 +0530

    Mob Framework

---
+++ src/com/aadi/rain/entity/mob/Mob.java
@@ -0,0 +1,22 @@
+package com.aadi.rain.entity.mob;
+
+import com.aadi.rain.entity.Entity;
+import com.aadi.rain.graphics.Sprite;
+
+public abstract class Mob extends Entity {
+
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+
+	public void move() {
+	}
+
+	public void update() {
+	}
+
+	private boolean collision() {
+		return false;
+	}
+
+}
