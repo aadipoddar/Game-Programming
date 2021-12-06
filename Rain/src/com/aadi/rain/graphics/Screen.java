e5a7aef62e4540d0a4927b8bb794b1f520f175cf
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-06 16:15:35 +0530

    Finishing Player Graphics

--- Rain/src/com/aadi/rain/graphics/Screen.java
+++ Rain/src/com/aadi/rain/graphics/Screen.java
@@ -61,7 +61,9 @@ public class Screen {
 
 				if (xa < -16 || xa >= width || ya < 0 || ya >= height) break;
 				if (xa < 0) xa = 0;
-				pixels[xa + ya * width] = sprite.pixels[x + y * 16];
+				int col = sprite.pixels[x + y * 16];
+
+				if (col != 0xffff00ff) pixels[xa + ya * width] = col;
 			}
 		}
 	}
