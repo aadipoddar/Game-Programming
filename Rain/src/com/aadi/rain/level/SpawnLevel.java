b49f292519cde448b4951523a10ecc50e773e9af
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-10 21:52:15 +0530

    Getting Tiles the Right Way

--- Rain/src/com/aadi/rain/level/SpawnLevel.java
+++ Rain/src/com/aadi/rain/level/SpawnLevel.java
@@ -5,11 +5,7 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import com.aadi.rain.level.tile.Tile;
-
 public class SpawnLevel extends Level {
-
-	private int[] levelPixels;
 
 	public SpawnLevel(String path) {
 		super(path);
@@ -21,10 +17,8 @@ public class SpawnLevel extends Level {
 			int w = image.getWidth();
 			int h = image.getHeight();
 
-			tiles = new Tile[w * h];
-
-			levelPixels = new int[w * h];
-			image.getRGB(0, 0, w, h, levelPixels, 0, w);
+			tiles = new int[w * h];
+			image.getRGB(0, 0, w, h, tiles, 0, w);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Exception! Could not load level file!");
@@ -32,11 +26,6 @@ public class SpawnLevel extends Level {
 	}
 
 	protected void generateLevel() {
-		for (int i = 0; i < levelPixels.length; i++) {
-			if (levelPixels[i] == 0xff00ff00) tiles[i] = Tile.grass;
-			if (levelPixels[i] == 0xffffff00) tiles[i] = Tile.flower;
-			if (levelPixels[i] == 0xff7f7f00) tiles[i] = Tile.rock;
-		}
 	}
 
 }
