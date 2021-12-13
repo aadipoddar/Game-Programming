6bab2e1cc0e1ec648eceb706bb471e4a560afd42
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-13 19:58:50 +0530

    Building the Spawn Level

--- Rain/src/com/aadi/rain/Game.java
+++ Rain/src/com/aadi/rain/Game.java
@@ -44,7 +44,7 @@ public class Game extends Canvas implements Runnable {
 		screen = new Screen(width, height);
 		frame = new JFrame();
 		key = new Keyboard();
-		level = new SpawnLevel("/textures/level.png");
+		level = new SpawnLevel("/levels/level.png");
 		player = new Player(6 * 16, 4 * 16, key);
 
 		addKeyListener(key);
