4279529335c5c6987e5161526c0cc8ef60d2c346
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-13 19:45:27 +0530

    Defining / Creating Spawn Level Tiles

--- Rain/src/com/aadi/rain/level/tile/Tile.java
+++ Rain/src/com/aadi/rain/level/tile/Tile.java
@@ -2,6 +2,11 @@ package com.aadi.rain.level.tile;
 
 import com.aadi.rain.graphics.Screen;
 import com.aadi.rain.graphics.Sprite;
+import com.aadi.rain.level.tile.spawn_level.SpawnFloorTile;
+import com.aadi.rain.level.tile.spawn_level.SpawnGrassTile;
+import com.aadi.rain.level.tile.spawn_level.SpawnHedgeTile;
+import com.aadi.rain.level.tile.spawn_level.SpawnWallTile;
+import com.aadi.rain.level.tile.spawn_level.SpawnWaterTile;
 
 public class Tile {
 
@@ -12,6 +17,13 @@ public class Tile {
 	public static Tile flower = new FlowerTile(Sprite.flower);
 	public static Tile rock = new RockTile(Sprite.rock);
 	public static Tile voidTile = new VoidTile(Sprite.voidSprite);
+
+	public static Tile spawn_grass = new SpawnGrassTile(Sprite.spawn_grass);
+	public static Tile spawn_hedge = new SpawnHedgeTile(Sprite.spawn_hedge);
+	public static Tile spawn_water = new SpawnWaterTile(Sprite.spawn_water);
+	public static Tile spawn_wall1 = new SpawnWallTile(Sprite.spawn_wall1);
+	public static Tile spawn_wall2 = new SpawnWallTile(Sprite.spawn_wall2);
+	public static Tile spawn_floor = new SpawnFloorTile(Sprite.spawn_floor);
 
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
