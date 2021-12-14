1e45112efec62baabaccb1e3d8379b2d6e79fbae
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-14 19:36:12 +0530

    Particle Physics!

--- Rain/src/com/aadi/rain/entity/particle/Particle.java
+++ Rain/src/com/aadi/rain/entity/particle/Particle.java
@@ -11,7 +11,8 @@ public class Particle extends Entity {
 	private int life;
 	private int time = 0;
 
-	protected double xx, yy, xa, ya;
+	protected double xx, yy, zz;
+	protected double xa, ya, za;
 
 	public Particle(int x, int y, int life) {
 		this.x = x;
@@ -23,8 +24,10 @@ public class Particle extends Entity {
 		this.life = life + (random.nextInt(20) - 10);
 		sprite = Sprite.particle_normal;
 
-		this.xa = random.nextGaussian();
+		this.xa = random.nextGaussian() + 1.8;
+		if (this.xa < 0) xa = 0.1;
 		this.ya = random.nextGaussian();
+		this.zz = random.nextFloat() + 2.0;
 	}
 
 	public void update() {
@@ -33,12 +36,22 @@ public class Particle extends Entity {
 		if (time >= 7400) time = 0;
 		if (time > life) remove();
 
+		za -= 0.1;
+
+		if (zz < 0) {
+			zz = 0;
+			za *= -0.55;
+			xa *= 0.4;
+			ya *= 0.4;
+		}
+
 		this.xx += xa;
 		this.yy += ya;
+		this.zz += za;
 	}
 
 	public void render(Screen screen) {
-		screen.renderSprite((int) xx, (int) yy, sprite, true);
+		screen.renderSprite((int) xx - 12, (int) yy - (int) zz, sprite, true);
 	}
 
 }
