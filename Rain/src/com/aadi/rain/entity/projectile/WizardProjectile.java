f0bac7e8052246199c407984c3e904e4bdfad134
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-14 20:09:21 +0530

    Pixel-Perfect Collision!

--- Rain/src/com/aadi/rain/entity/projectile/WizardProjectile.java
+++ Rain/src/com/aadi/rain/entity/projectile/WizardProjectile.java
@@ -20,7 +20,7 @@ public class WizardProjectile extends Projectile {
 	}
 
 	public void update() {
-		if (level.tileCollision(x, y, nx, ny, 7)) {
+		if (level.tileCollision((int) (x + nx), (int) (y + nx), 7, 5, 4)) {
 			level.add(new ParticleSpawner((int) x, (int) y, 44, 50, level));
 			remove();
 		}
