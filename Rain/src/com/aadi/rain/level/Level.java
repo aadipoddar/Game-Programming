f7a798c12a58a8b038efc56414fc3a66a81587bb
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-14 16:40:48 +0530

    Projectile Collision

--- Rain/src/com/aadi/rain/level/Level.java
+++ Rain/src/com/aadi/rain/level/Level.java
@@ -54,6 +54,17 @@ public class Level {
 	private void time() {
 	}
 
+	public boolean tileCollision(double x, double y, double xa, double ya, int size) {
+		boolean solid = false;
+		for (int c = 0; c < 4; c++) {
+			int xt = (((int) x + (int) xa) + c % 2 * size * 2 - 12) / 16;
+			int yt = (((int) y + (int) ya) + c / 2 * size + 2) / 16;
+			if (getTile(xt, yt).solid()) solid = true;
+		}
+
+		return solid;
+	}
+
 	public void render(int xScroll, int yScroll, Screen screen) {
 		screen.setOffset(xScroll, yScroll);
 
@@ -82,6 +93,7 @@ public class Level {
 	}
 
 	public void addProjectile(Projectile p) {
+		p.init(this);
 		projectiles.add(p);
 	}
 
